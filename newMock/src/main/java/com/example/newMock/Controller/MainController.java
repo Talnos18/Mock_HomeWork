894cf2996f91f928ac7685d8d3a9e45ba0781832
Ilya Ali-Zade <160397567+Talnos18@users.noreply.github.com>
2024-03-20 17:38:32 +0400

    Add files via upload

---
+++ newMock/src/main/java/com/example/newMock/Controller/MainController.java
@@ -0,0 +1,79 @@
+package com.example.newMock.Controller;
+
+import com.example.newMock.Model.RequestDTO;
+import com.example.newMock.Model.ResponseDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.*;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Random;
+
+@RestController
+public class MainController {
+
+    private Logger log = LoggerFactory.getLogger(MainController.class);
+
+    ObjectMapper mapper = new ObjectMapper();
+    Random random = new Random();
+
+    @PostMapping(
+            value = "/info/postBalances",
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Object postBalances(@RequestBody RequestDTO requestDTO){
+        try {
+            String clientId = requestDTO.getClientId();
+            char firstDigit = clientId.charAt(0);
+            BigDecimal maxLimit;
+            String rqUID = requestDTO.getRqUID();
+
+            if (firstDigit == '8') {
+                maxLimit = new BigDecimal(2000);
+            } else if (firstDigit == '9') {
+                maxLimit = new BigDecimal(1000);
+            } else {
+                maxLimit = new BigDecimal(10000);
+            }
+
+            BigDecimal balance = new BigDecimal(random.nextDouble()).multiply(maxLimit);
+            balance = balance.setScale(2, RoundingMode.HALF_UP);
+
+            ResponseDTO responseDTO = new ResponseDTO();
+
+            responseDTO.setRqUID(rqUID);
+            responseDTO.setClientId(clientId);
+            responseDTO.setAccount(requestDTO.getAccount());
+
+            String currency;
+            if (firstDigit == '8') {
+                currency = "US";
+            } else if (firstDigit == '9') {
+                currency = "EU";
+            } else {
+                currency = "RUB";
+            }
+
+            responseDTO.setCurrency(currency);
+            responseDTO.setBalance(balance);
+            responseDTO.setMaxLimit(maxLimit);
+
+            log.error("********** RequestDTO **********" + mapper.writerWithDefaultPrettyPrinter().writeValueAsString(requestDTO));
+            log.error("********** ResponseDTO **********" + mapper.writerWithDefaultPrettyPrinter().writeValueAsString(responseDTO));
+
+            return responseDTO;
+
+        } catch (Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+}
